fbe818e0cf421195156ec2cb1d50f0cd9c2fc882
Shahanawaz <shahanawazkhan579@gmail.com>
2023-09-11 10:45:49 +0530

    Added hover effect

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,6 +1,6 @@
 const ServiceCard = ({ imgURL, label, subtext }) => {
   return (
-    <div className="flex-1 sm:w-[350px] sm:min-w-[350px] w-full rounded-[20px] shadow-3xl px-10 py-16 hover:bg-coral-red hover:text-white cursor-pointer">
+    <div className="flex-1 sm:w-[350px] sm:min-w-[350px] w-full rounded-[20px] shadow-3xl px-10 py-16 hover:bg-coral-red hover:text-white cursor-pointer transition-all duration-500 ease-in-out">
       <div className="w-11 h-11 bg-coral-red flex justify-center items-center rounded-full">
         <img src={imgURL} alt={label} width={24} height={24} />
       </div>
